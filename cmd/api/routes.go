23b13b3f9990ce1dc091c75159fa7db449f42cb3
Joel Ical <2005113038@gmail.com>
2022-12-04 10:44:25 -0600

    wrap our routes with  requireActivatedUser middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,12 +17,12 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedesponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/photo", app.listPhotoHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/photo", app.createPhotoHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/photo", app.requireActivatedUser(app.listPhotoHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/photo", app.requireActivatedUser(app.createPhotoHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/photo/:id", app.showPhotoHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/photo/:id", app.updatePhotoHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/photo/:id", app.deletePhotoHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/photo/:id", app.requireActivatedUser(app.showPhotoHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/photo/:id", app.requireActivatedUser(app.updatePhotoHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/photo/:id", app.requireActivatedUser(app.deletePhotoHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
